e1f90ca973912a3bffbec8e36db4a1530ce8b8cd
MonicaShishodia <monicashishodia@gmail.com>
2016-07-15 16:10:37 +0530

    NUnit Test case

---
+++ NUnit/NUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+
+namespace NUnit
+{
+    [TestFixture]
+   public class NUnitTest
+    {
+        IWebDriver driver = null;
+        [SetUp]
+        public void OpenURL()
+        {
+            driver = new FirefoxDriver();
+           
+        }
+        [Test]
+        public void NunitSeleniumTest()
+        {
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost/TestCI");
+                IWebElement element = driver.FindElement(By.Id("txtBox"));
+                element.SendKeys("Test");
+            }
+            catch (Exception e)
+            { }
+            finally { }
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Close();
+        }
+    }
+}
